6d8e6c4164b00ca49427a47b1065357f8f47b6d6
James Liauw <jamesliauw@163.com>
2019-03-04 20:19:34 +0800

    feat: add fun structs

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -11,8 +11,73 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-	return Container(
-	   child: widget.child,
+	return Scaffold(
+		appBar: AppBar(
+			backgroundColor: Colors.transparent,
+			elevation: 0.0,
+			leading: IconButton(
+				icon: Icon(Icons.arrow_back),
+				color: Colors.black,
+				onPressed: () {
+					Navigator.of(context).pop();
+				},
+			),
+			actions: <Widget>[
+				IconButton(icon: Icon(Icons.more_vert), onPressed: (){})
+			],
+		),
+		body: ListView(
+			children: <Widget>[
+				Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: <Widget>[
+						this.buildHero(),
+						this.buildSizeBox(25.0),
+						this.buildStewart(),
+						this.buildSizeBox(4.0),
+						this.buildSanJose(),
+						this.buildFlowers(),
+						this.buildTableChart(),
+						this.buildImages(),
+						this.buildInfoDetail(),
+						this.buildImages(),
+						this.buildInfoDetail()
+					],
+				)
+			],
+		),
 	);
+  }
+
+  Widget buildHero() {
+
+  }
+
+  Widget buildStewart() {
+
+  }
+
+  Widget buildSanJose() {
+
+  }
+
+  Widget buildFlowers() {
+
+  }
+
+  Widget buildTableChart() {
+
+  }
+
+  Widget buildInfoDetail() {
+
+  }
+
+  Widget buildImages() {
+
+  }
+
+  Widget buildSizeBox(double widthValue) {
+	  return SizedBox(height: widthValue);
   }
 }
