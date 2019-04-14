5d1f37024602eb020101443cf04af8ebc3cac3f7
James Liauw <jamesliauw@163.com>
2019-04-14 12:23:27 +0800

    feat: add cross

--- lib/pages/PageCircler.dart
+++ lib/pages/PageCircler.dart
@@ -37,15 +37,77 @@ class _CirclerState extends State<PageCircler> {
 							this.buildScrollImageItem('assets/p6.jpg', 'assets/p2.jpg', 'Sanfrisco travel details'),
 						],
 					),
-				)
+				),
 
 				// 第3行
+				Padding(
+					padding: EdgeInsets.fromLTRB(0.0, 55.0, 0.0, 0.0),
+					child: Column(
+						crossAxisAlignment: CrossAxisAlignment.start,
+						children: <Widget>[
+							Text('To DO:', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 200.0,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0,
+                  padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                  children: <Widget>[
+                    this.buildGridIconView('98', 'Airbnbs', Colors.orangeAccent),
+                    this.buildGridIconView('56', 'Attractions', Colors.lightBlue),
+                    this.buildGridIconView('34', 'Experiences', Colors.blueAccent),
+                    this.buildGridIconView('09', 'Restaurants', Colors.limeAccent),
+                  ],
+                ),
+              )
+						],
+					),
+				)
 
 				// 第四行
 			],
 		);
   }
 
+  /// 构建圆角图标
+  Widget buildGridIconView(String startCount, String hotelName, Color colorValue) {
+	  return Container(
+		  width: 180.0,
+		  height: 120.0,
+		  decoration: BoxDecoration(
+			  color: colorValue,
+			  borderRadius: BorderRadius.circular(20.0)
+		  ),
+		  child: Column(
+			  children: <Widget>[
+				  Container(
+					  width: 60.0,
+					  height: 60.0,
+					  decoration: BoxDecoration(
+						  color: Colors.white,
+						  borderRadius: BorderRadius.circular(50.0),
+						  boxShadow: [
+							  BoxShadow(
+								  color: Colors.grey,
+								  offset: Offset(0.0, 3.0),
+								  blurRadius: 3.0,
+								  spreadRadius: 3.0
+							  )
+						  ]
+					  ),
+					  child: Center(
+						  child: Text( startCount, style: TextStyle(fontSize: 20.0, color: colorValue)),
+					  ),
+				  ),
+				  SizedBox(height: 15.0),
+				  Text(hotelName, style: TextStyle(fontSize: 20.0, color: Colors.white))
+			  ],
+		  ),
+	  );
+  }
+
+  /// 滚动图片
   Widget buildScrollImageItem(String coverUrl, String avataUrl, String titleDesc) {
 	  return Column(
 		  crossAxisAlignment: CrossAxisAlignment.start,
