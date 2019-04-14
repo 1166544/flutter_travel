a98e45a21288115c5c58bb6f673b9a9ca2152db9
James Liauw <jamesliauw@163.com>
2019-04-14 13:31:56 +0800

    feat: add liner color

--- lib/pages/PageCircler.dart
+++ lib/pages/PageCircler.dart
@@ -48,16 +48,21 @@ class _CirclerState extends State<PageCircler> {
 							Text('To DO:', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
               Container(
                 width: MediaQuery.of(context).size.width,
-                height: 200.0,
+                height: 280.0,
                 child: GridView.count(
                   crossAxisCount: 2,
-                  crossAxisSpacing: 10.0,
-                  padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                  crossAxisSpacing: 23.0,
+                  mainAxisSpacing: 23.0,
+                  childAspectRatio: (90 / 65),  // CELL宽高
+                  controller: new ScrollController(keepScrollOffset: false),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  padding: EdgeInsets.fromLTRB(0.0, 10.0, 12.0, 0.0),
                   children: <Widget>[
-                    this.buildGridIconView('98', 'Airbnbs', Colors.orangeAccent),
-                    this.buildGridIconView('56', 'Attractions', Colors.lightBlue),
-                    this.buildGridIconView('34', 'Experiences', Colors.blueAccent),
-                    this.buildGridIconView('09', 'Restaurants', Colors.limeAccent),
+                    this.buildGridIconView('98', 'Airbnbs', Colors.orangeAccent, Colors.orange),
+                    this.buildGridIconView('56', 'Attractions', Colors.lightBlue[300], Colors.blue),
+                    this.buildGridIconView('34', 'Experiences', Colors.purple, Colors.deepPurple),
+                    this.buildGridIconView('09', 'Restaurants', Colors.grey.withOpacity(0.5), Colors.grey),
                   ],
                 ),
               )
@@ -71,39 +76,53 @@ class _CirclerState extends State<PageCircler> {
   }
 
   /// 构建圆角图标
-  Widget buildGridIconView(String startCount, String hotelName, Color colorValue) {
+  Widget buildGridIconView(String startCount, String hotelName, Color colorValue, Color shapeColor) {
 	  return Container(
-		  width: 180.0,
-		  height: 120.0,
 		  decoration: BoxDecoration(
 			  color: colorValue,
-			  borderRadius: BorderRadius.circular(20.0)
+			  borderRadius: BorderRadius.circular(20.0),
+        gradient: RadialGradient(
+          center: Alignment(-0.8, -0.5),
+          radius: 0.8,
+          colors: [
+            colorValue,
+            shapeColor
+          ],
+          stops: [
+            0.0,
+            0.9
+          ]
+        )
 		  ),
-		  child: Column(
-			  children: <Widget>[
-				  Container(
-					  width: 60.0,
-					  height: 60.0,
-					  decoration: BoxDecoration(
-						  color: Colors.white,
-						  borderRadius: BorderRadius.circular(50.0),
-						  boxShadow: [
-							  BoxShadow(
-								  color: Colors.grey,
-								  offset: Offset(0.0, 3.0),
-								  blurRadius: 3.0,
-								  spreadRadius: 3.0
-							  )
-						  ]
-					  ),
-					  child: Center(
-						  child: Text( startCount, style: TextStyle(fontSize: 20.0, color: colorValue)),
-					  ),
-				  ),
-				  SizedBox(height: 15.0),
-				  Text(hotelName, style: TextStyle(fontSize: 20.0, color: Colors.white))
-			  ],
-		  ),
+		  child: Padding(
+        padding: EdgeInsets.fromLTRB(25.0, 20.0, 0.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: 40.0,
+              height: 40.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: shapeColor,
+                    offset: Offset(0.0, 3.0),
+                    blurRadius: 3.0,
+                    spreadRadius: 3.0
+                  )
+                ],
+              ),
+              child: Center(
+                child: Text( startCount, style: TextStyle(fontSize: 17.0, color: shapeColor, fontWeight: FontWeight.bold)),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(hotelName, style: TextStyle(fontSize: 21.0, color: Colors.white))
+          ],
+        ),
+      ),
 	  );
   }
 
