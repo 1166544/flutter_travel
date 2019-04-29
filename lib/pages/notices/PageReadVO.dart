8392483475ce248540c457fcf3c18a2ec8246811
James Liauw <jamesliauw@163.com>
2019-04-29 09:35:38 +0800

    feat: add common

--- lib/pages/notices/PageReadVO.dart
+++ lib/pages/notices/PageReadVO.dart
@@ -1,8 +1,13 @@
 import 'package:flutter_travel/pages/notices/PageNoticeVO.dart';
 
+/// 展示用消息体
 class PageReadVO {
+    // 构造体
 	PageReadVO({this.dateTitle, this.readList});
 
+	/// 日期标题
 	String dateTitle;
+
+	/// 标题列表
 	List<PageNoticeVO> readList;
 }
