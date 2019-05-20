8b23f45bb1978b3a85157d85ccbe2c75049df262
James Liauw <jamesliauw@163.com>
2019-05-20 10:06:09 +0800

    feat: add common

--- lib/pages/notices/PageNoticeVO.dart
+++ lib/pages/notices/PageNoticeVO.dart
@@ -2,8 +2,13 @@
 class PageNoticeVO {
 	PageNoticeVO({this.subTitle, this.description, this.isReaded = false});
 
+	/// 子标题
 	String subTitle;
+
+	/// 描述
 	String description;
+
+	/// 是否已读
 	bool isReaded;
 
 }
