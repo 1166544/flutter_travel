24543c0c4c92a76c485d54109ca28b5a2f41e703
James Liauw <jamesliauw@163.com>
2019-11-16 19:34:13 +0800

    feat: abs cover

--- lib/pages/modules/circler/views/CirclerScroll.dart
+++ lib/pages/modules/circler/views/CirclerScroll.dart
@@ -51,9 +51,7 @@ class CirclerScroll extends StatelessWidget {
 						item.title)
 			);
 
-			if (i % 2 != 0) {
-				lsitWidget.add(SizedBox(width: 15.0));
-			}
+			lsitWidget.add(SizedBox(width: 15.0));
 		}
 
 		return lsitWidget;
@@ -63,66 +61,67 @@ class CirclerScroll extends StatelessWidget {
 	Widget buildScrollImageItem(
 		String coverUrl, String avataUrl, String titleDesc) {
 			
-			return Container(
-				width: 290.0,
-				height: 180.0,
+		return Container(
+			width: 290.0,
+			height: 180.0,
+			decoration: BoxDecoration(
+				image: DecorationImage(image: NetworkImage(coverUrl), fit: BoxFit.cover),
+				borderRadius: BorderRadius.only(
+					topLeft: Radius.circular(10.0),
+					topRight: Radius.circular(10.0),
+					bottomLeft: Radius.circular(10.0),
+					bottomRight: Radius.circular(10.0),
+				)),
+			child: Container(
+				padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 10.0),
 				decoration: BoxDecoration(
-					image: DecorationImage(image: NetworkImage(coverUrl), fit: BoxFit.cover),
-					borderRadius: BorderRadius.only(
-						topLeft: Radius.circular(10.0),
-						topRight: Radius.circular(10.0),
-						bottomLeft: Radius.circular(10.0),
-						bottomRight: Radius.circular(10.0),
-					)),
-				child: Container(
-					padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 10.0),
-					decoration: BoxDecoration(
-						gradient: LinearGradient(
-							begin: Alignment.topCenter,
-							end: Alignment.bottomCenter,
-							stops: [0.0, 0.5, 0.65, 0.9],
-							colors: [
-								Colors.black.withOpacity(0.0),
-								Colors.black.withOpacity(0.3),
-								Colors.black.withOpacity(0.5),
-								Colors.black.withOpacity(0.7),
-							],
-						),
-						borderRadius: BorderRadius.only(
-						topLeft: Radius.circular(10.0),
-						topRight: Radius.circular(10.0),
-						bottomLeft: Radius.circular(10.0),
-						bottomRight: Radius.circular(10.0),
-					)
-					),
-					child: Row(
-						mainAxisAlignment: MainAxisAlignment.start,
-						crossAxisAlignment: CrossAxisAlignment.end,
-						children: <Widget>[
-							Container(
-								width: 40.0,
-								height: 40.0,
-								decoration: BoxDecoration(
-								image: DecorationImage(
-										image: NetworkImage(avataUrl), fit: BoxFit.cover),
-										borderRadius: BorderRadius.only(
-											topLeft: Radius.circular(7.0),
-											topRight: Radius.circular(7.0),
-											bottomLeft: Radius.circular(7.0),
-											bottomRight: Radius.circular(7.0),
-								)),
-							),
-							SizedBox(width: 15.0),
-							Container(
-								height: 40.0,
-								child: Text(titleDesc,
-									maxLines: 2,
-									softWrap: true,
-									style: TextStyle(fontWeight: FontWeight.normal, fontSize: 17.0, color: Colors.white)))
+					gradient: LinearGradient(
+						begin: Alignment.topCenter,
+						end: Alignment.bottomCenter,
+						stops: [0.0, 0.5, 0.65, 0.9],
+						colors: [
+							Colors.black.withOpacity(0.0),
+							Colors.black.withOpacity(0.3),
+							Colors.black.withOpacity(0.5),
+							Colors.black.withOpacity(0.7),
 						],
 					),
+					borderRadius: BorderRadius.only(
+					topLeft: Radius.circular(10.0),
+					topRight: Radius.circular(10.0),
+					bottomLeft: Radius.circular(10.0),
+					bottomRight: Radius.circular(10.0),
+				)
 				),
-			);
+				child: Row(
+					mainAxisAlignment: MainAxisAlignment.start,
+					crossAxisAlignment: CrossAxisAlignment.end,
+					children: <Widget>[
+						Container(
+							width: 40.0,
+							height: 40.0,
+							decoration: BoxDecoration(
+							image: DecorationImage(
+									image: NetworkImage(avataUrl), fit: BoxFit.cover),
+									borderRadius: BorderRadius.only(
+										topLeft: Radius.circular(7.0),
+										topRight: Radius.circular(7.0),
+										bottomLeft: Radius.circular(7.0),
+										bottomRight: Radius.circular(7.0),
+							)),
+						),
+						SizedBox(width: 15.0),
+						Container(
+							width: 210.0,
+							height: 40.0,
+							child: Text(titleDesc,
+								maxLines: 2,
+								softWrap: true,
+								style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15.0, color: Colors.white)))
+					],
+				),
+			),
+		);
 
 	}
 }
