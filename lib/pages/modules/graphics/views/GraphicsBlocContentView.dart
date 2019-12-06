07581172484156d4e00207d7b18ab478fea2e497
James Liauw <jamesliauw@163.com>
2019-12-06 18:15:48 +0800

    feat: add logout button

--- lib/pages/modules/graphics/views/GraphicsBlocContentView.dart
+++ lib/pages/modules/graphics/views/GraphicsBlocContentView.dart
@@ -145,17 +145,7 @@ class _GraphicsBlocContentViewState extends State<GraphicsBlocContentView> with 
 			child: Column(
 				crossAxisAlignment: CrossAxisAlignment.start,
 				children: <Widget>[
-					Text(
-						'Reported Avg',
-						style: TextStyle(
-							fontSize: 16.0,
-							color: Color(0xFF252949),
-							decorationColor: Color(0xffffefdb),
-							decorationThickness: 4.0,
-							fontWeight: FontWeight.bold,
-							decoration: TextDecoration.underline
-						)
-					),
+					this.getUnderlineText('Reported Ava'),
 					SizedBox(height: 15.0),
 					Text(
 						'2 tables away 6-7 hours nor more than 8 tables \nper day',
@@ -207,18 +197,203 @@ class _GraphicsBlocContentViewState extends State<GraphicsBlocContentView> with 
 	
 	/// 中部实现信息
 	Widget buildMiddleVisitorStucts(AsyncSnapshot<ModelGallery> snapshot) {
-		return Text('test');
+		return Container(
+			padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 40.0),
+			decoration: BoxDecoration(
+				color: Color(0xFFebeff9)
+			),
+			child: Row(
+				crossAxisAlignment: CrossAxisAlignment.center,
+				mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+				children: <Widget>[
+					this.getVisitorItem(90, 'Unknow', 0.0),
+					this.getVisitorItem(11, 'Famillar', 0.4),
+					this.getVisitorItem(6, 'Excellent', 1.0),
+				]
+			)
+		);
 	}
 	
 	/// 中部访问信息
 	Widget buildMiddlePresciptionStucts(AsyncSnapshot<ModelGallery> snapshot) {
-		return Text('test');
+		return Container(
+			padding: EdgeInsets.fromLTRB(20.0, 55.0, 20.0, 45.0),
+			child: Column(
+				crossAxisAlignment: CrossAxisAlignment.start,
+				children: <Widget>[
+					this.getUnderlineText('Prescription'),
+					SizedBox(height: 15.0),
+					Container(
+						decoration: BoxDecoration(
+							color: Color(0xFFe1e6f0)
+						),
+						padding: EdgeInsets.all(18.0),
+						child: Column(
+							children: <Widget>[
+								this.getPresciptonItem('Learned phrases', '12'),
+								SizedBox(height: 12),
+								this.getPresciptonItem('Phrases left to learn', '36'),
+								SizedBox(height: 12),
+								this.getPresciptonItem('Total learning time', '8.2h'),
+							],
+						),
+					),
+					SizedBox(height: 15.0),
+					Text(
+						'730',
+						style: TextStyle(
+							fontSize: 39.0,
+							color: Colors.black,
+							fontWeight: FontWeight.bold
+						)
+					),
+					Row(
+						children: <Widget>[
+							Text(
+								'visitors last 7 days',
+								style: TextStyle(
+									fontSize: 19.0,
+									color: Color(0xFF9395a3)
+								)
+							),
+							Icon(Icons.arrow_right, color: Color(0xFF9395a3))
+						],
+					)
+				]
+			)
+		);
 	}
 	
 	/// 底部按钮
 	Widget buildButtonStucts(AsyncSnapshot<ModelGallery> snapshot) {
-		return Text('test');
+		// return Text('test');
+		return Container(
+			padding: EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 40.0),
+			child: GestureDetector(
+				onTap: () {
+					// hole
+				},
+				child: Container(
+					decoration: BoxDecoration(
+						color: Color(0xFF5e82f5),
+						borderRadius: BorderRadius.circular(9.0)
+					),
+					padding: EdgeInsets.all(10.0),
+					child: Center(
+						child: Text(
+							'Logout system here',
+							style: TextStyle(
+								fontSize: 19.0,
+								fontWeight: FontWeight.bold,
+								color: Colors.white
+							)
+						),
+					),
+				)
+			)
+		);
 	}
 
+	/// 访问人数显示项
+	Widget getVisitorItem(int score, String desc, double process) {
+		final double cellWidth = 100.0;
+		return Container(
+			decoration: BoxDecoration(
+				color: Colors.white,
+				borderRadius: BorderRadius.circular(10.0)
+			),
+			width: cellWidth,
+			height: 130.0,
+			padding: EdgeInsets.only(left: 15.0, right: 15.0),
+			child: Column(
+				mainAxisAlignment: MainAxisAlignment.center,
+				children: <Widget>[
+					Text(
+						score.toString(),
+						style: TextStyle(
+							fontSize: 28.0,
+							fontWeight: FontWeight.bold,
+							color: score > 50 ? Color(0xFF6d6d8d) : Colors.black
+						)
+					),
+					SizedBox(height: 15.0),
+					Text(
+						desc,
+						style: TextStyle(
+							fontSize: 15.0,
+							fontWeight: FontWeight.bold,
+							color: score > 50 ? Color(0xFF6d6d8d) : Colors.black
+						)
+					),
+					SizedBox(height: 15.0),
+					Stack(
+						children: <Widget>[
+							Container(
+								height: 4.0,
+								decoration: BoxDecoration(
+									borderRadius: BorderRadius.circular(18.0),
+									color: Color(0xFFdce5e3)
+								),
+							),
+							Container(
+								width: cellWidth * process,
+								height: 4.0,
+								decoration: BoxDecoration(
+									borderRadius: BorderRadius.circular(18.0),
+									gradient: LinearGradient(
+										begin: Alignment.centerLeft,
+										end: Alignment.centerRight,
+										colors: [
+											Color(0xFF5e82f5),
+											Color(0xFF5e82f5)
+										]
+									)
+								),
+							),
+						],
+					)
+				],
+			),
+		);
+	}
+
+	/// 实现横条内容
+	Widget getPresciptonItem(String title, String subTitle) {
+		return Row(
+			children: <Widget>[
+				Text(
+					title,
+					style: TextStyle(
+						fontSize: 12.0,
+						color: Colors.black
+					)
+				),
+				Spacer(),
+				Text(
+					subTitle,
+					style: TextStyle(
+						fontSize: 12.0,
+						color: Colors.black,
+						fontWeight: FontWeight.bold
+					)
+				)
+			],
+		);
+	}
+
+	/// 带黄底横线标题
+	Widget getUnderlineText(String content) {
+		return Text(
+			content,
+			style: TextStyle(
+				fontSize: 16.0,
+				color: Color(0xFF252949),
+				decorationColor: Color(0xffffefdb),
+				decorationThickness: 4.0,
+				fontWeight: FontWeight.bold,
+				decoration: TextDecoration.underline
+			)
+		);
+	}
 	
 }
