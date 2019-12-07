a0a989ae8ec1efa14073e7936d37ab19a96b6da7
James Liauw <jamesliauw@163.com>
2019-12-07 14:05:16 +0800

    fix: color don't has constructor

--- lib/styles/StyleColors.dart
+++ lib/styles/StyleColors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Map<String, Color> colorStyles = {
     'primary_dark': Colors.blue[700],
-    'primary': Color(0xFFebeff8),
+    'primary': Colors.lightBlue,
     'ligth_font': Colors.black54,
     'gray': Colors.black45,
     'white': Colors.white
