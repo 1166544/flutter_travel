2ad625711925c3f9a4f5b3e65ece1c360e0d8555
James Liauw <jamesliauw@163.com>
2019-12-07 19:49:47 +0800

    feat: add search content

--- lib/pages/modules/circler/views/compnents/CirclerSearchContent.dart
+++ lib/pages/modules/circler/views/compnents/CirclerSearchContent.dart
@@ -5,6 +5,8 @@ import 'package:flutter_travel/pages/common/CommonTravelItem.dart';
 import 'package:flutter_travel/pages/modules/circler/views/compnents/CirclerSearchBar.dart';
 import 'package:flutter_travel/pages/modules/graphics/blocs/GraphicsBlocMyInfo.dart';
 import 'package:flutter_travel/pages/modules/graphics/models/GraphicsBlocModel.dart';
+import 'package:flutter_travel/pages/modules/notices/NoticePageVO.dart';
+import 'package:flutter_travel/pages/modules/notices/NoticeReadVO.dart';
 
 /// 搜索强结果视图
 class CirclerSearchContent extends StatefulWidget {
@@ -69,7 +71,195 @@ class _CirclerSearchContentState extends State<CirclerSearchContent> with Common
 						print(val)
 					}),
 					// 搜索结果列表
+					this.buildNotifcationList()
+				],
+			),
+		);
+	}
 
+	/// 通知列表
+	Widget buildNotifcationList() {
+		List<NoticePageVO> subList = [
+			new NoticePageVO(subTitle: 'New to Flutter', description: "Bookmark the API reference docs for the Flutter framework."),
+		];
+
+		List<NoticeReadVO> readList = [
+			new NoticeReadVO(dateTitle: 'Mon 1, Nov', readList: subList),
+			new NoticeReadVO(dateTitle: 'Sep 2, Dec', readList: subList),
+		];
+
+		List<Widget> readRenderList = [];
+		for (NoticeReadVO item in readList) {
+			readRenderList.add(
+				this.builRenderReadItem(item)
+			);
+		}
+		return Column(
+			children: readRenderList,
+		);
+	}
+
+	/// 构建消息列表项
+	/// * [PageReadVO item] 消息体
+	Widget builRenderReadItem(NoticeReadVO item) {
+
+		List<Widget> readSubRenderList = [];
+
+		// 加入预编译数据
+		for (NoticePageVO item in item.readList) {
+			readSubRenderList.add(this.buildSubReadRenderItem(item));
+		}
+
+		return Padding(
+			padding: EdgeInsets.only(left: 10.0),
+			child: Row(
+				crossAxisAlignment: CrossAxisAlignment.start,
+				mainAxisAlignment: MainAxisAlignment.start,
+				children: <Widget>[
+					Container(
+						width: 80.0,
+						child: Text(
+							item.dateTitle,
+							softWrap: true,
+							style: TextStyle(
+								fontSize: 12.0,
+								fontWeight: FontWeight.bold,
+							),
+						),
+					),
+					Column(
+						crossAxisAlignment: CrossAxisAlignment.start,
+						children: readSubRenderList
+					)
+				],
+			)
+		);
+	}
+
+	/// 消息列表项
+	/// * [PageNoticeVO item] 消息数据源
+	Widget buildSubReadRenderItem(NoticePageVO item) {
+		return Column(
+			crossAxisAlignment: CrossAxisAlignment.start,
+			children: <Widget>[
+					// 带圆圈标题行
+					Row(
+						children: <Widget>[
+							// 圆圈
+							Icon(Icons.check_circle_outline, size: 20.0, color: Colors.blue),
+							// 子标题
+							SizedBox(width: 12.0),
+							Container(
+								width: 230.0,
+								child: Text(
+									item.subTitle,
+									overflow: TextOverflow.ellipsis,
+									style: TextStyle(
+										fontFamily: 'Montserrat',
+										fontWeight: FontWeight.bold,
+										color: Colors.black87.withOpacity(0.6)
+									)
+								)
+							)
+						]
+					),
+					// 带虚线行
+					Row(
+						crossAxisAlignment: CrossAxisAlignment.start,
+						mainAxisAlignment: MainAxisAlignment.start,
+						children: <Widget>[
+							// 虚线
+							Container(
+								width: 10.0,
+								height: 120.0,
+								margin: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 30.0),
+								decoration: BoxDecoration(
+									color: Colors.white.withOpacity(0.0),
+									border: Border(right: BorderSide(color: Colors.grey.withOpacity(0.2), width: 1.0, style: BorderStyle.solid))
+								),
+							),
+							SizedBox(width: 22.0),
+							// 描述文本区
+							Column(
+								crossAxisAlignment: CrossAxisAlignment.end,
+								mainAxisAlignment: MainAxisAlignment.start,
+								children: <Widget>[
+									SizedBox(height: 8.0),
+									// 描述文本
+									Container(
+										width: 230.0,
+										child: Text(
+											item.description,
+											softWrap: true,
+											overflow: TextOverflow.ellipsis,
+											maxLines: 10,
+											style: TextStyle(
+												color: Colors.black.withOpacity(0.8),
+												fontSize: 12.0
+											),
+										)
+									),
+									SizedBox(height: 60.0),
+									// 操作按钮
+									this.buildOperationButton(),
+								],
+							)
+						]
+					)
+			],
+		);
+	}
+
+	/// 操作按钮区
+	Widget buildOperationButton() {
+		return Padding(
+			padding: EdgeInsets.only(bottom: 10.0),
+			child: Row(
+				children: <Widget>[
+					InkWell(
+						highlightColor: Colors.orangeAccent,
+						child: Container(
+							width: 110.0,
+							height: 30.0,
+							// 圆角背景按钮
+							decoration: BoxDecoration(
+								color: Colors.yellow,
+								shape: BoxShape.rectangle,
+								borderRadius: BorderRadius.circular(2.0)
+							),
+							child: Center(
+								child: Text('Make readed', style: TextStyle(
+											fontFamily: 'Montserrat',
+											fontSize: 12.0,
+											color: Colors.black
+								)),
+							),
+						),
+						onTap: () {},
+					),
+					SizedBox(width: 10.0),
+					// 描述删除按钮使用InkWell包装
+					InkWell(
+						highlightColor: Colors.grey,
+						child: Container(
+							width: 80.0,
+							height: 30.0,
+							decoration: BoxDecoration(
+								color: Colors.grey.withOpacity(0.5),
+								shape: BoxShape.rectangle,
+								borderRadius: BorderRadius.circular(2.0)
+							),
+							// 描述删除按钮
+							child: Center(
+								child: Text('DELETE', style: TextStyle(
+											fontFamily: 'Montserrat',
+											fontSize: 12.0,
+											color: Colors.black
+								)),
+							),
+						),
+						onTap: () {},
+					)
 				],
 			),
 		);
