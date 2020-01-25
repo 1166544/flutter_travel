35d9bc89f9a0d5ec89041c15b7dce08ebdccb6ea
James Liauw <jamesliauw@163.com>
2020-01-25 15:27:24 +0800

    feat: splash page structs

---
+++ lib/modules/splash/SplashPage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new SplashPageState();
+  }
+}
+
+class SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('data');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
