42057295f5fc4dc6e88a858ffe4a548551037068
James Liauw <jamesliauw@163.com>
2020-02-11 10:18:52 +0800

    feat: expand utils

--- lib/modules/common/CommonTimeFormate.dart
+++ lib/modules/common/CommonTimeFormate.dart
@@ -1,47 +1,20 @@
-/// 格式化时间组件
+import 'package:flutter_travel/modules/utils/Utils.dart';
+
+/// 格式化时间组件简化调用
 class CommonTimeFormate {
 
 	/// 返回时间截 2019/11/22 格式
 	String getDateText(dynamic dateNum) {
-		var dateNo = int.parse(dateNum);
-		var date = new DateTime.fromMillisecondsSinceEpoch(dateNo);
-		var dateStr = date.year.toString() + '/' + date.month.toString() + '/' + date.day.toString();
-
-		return dateStr;
+		return Utils.getDateText(dateNum);
 	}
 
 	/// 返回当年年份时间
 	String getNormalTime() {
-		var leftStr = DateTime.now().toLocal().toString();
-		var leftStrArr = leftStr.split(" ");
-
-		return leftStrArr[0];
+		return Utils.getNormalTime();
 	}
 
 	/// (字符串传入) 返回年月日 时分秒格式 2019/11/22 22:22
 	String getFullTime(String dateNum) {
-		var dateNo = int.parse(dateNum);
-		var date = new DateTime.fromMillisecondsSinceEpoch(dateNo);
-		var month = date.month.toString();
-		var day = date.day.toString();
-		var hours = date.hour.toString();
-		var minute = date.minute.toString();
-
-		if (month.length == 1) {
-			month = '0$month';
-		}
-		if (day.length == 1) {
-			day = '0$day';
-		}
-		if (hours.length == 1) {
-			hours = '0$hours';
-		}
-		if (minute.length == 1) {
-			minute = '0$minute';
-		}
-
-		var dateStr = date.year.toString() + '年' + month + '月' + day + '日 ' + hours + ':' + minute;
-
-		return dateStr;
+		return Utils.getFullTime(dateNum);
 	}
 }
