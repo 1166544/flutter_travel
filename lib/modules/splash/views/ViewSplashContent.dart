8ed7fc6e3f040c5ddd63f9209b15a963f6cf1fcb
James Liauw <jamesliauw@163.com>
2020-02-14 15:24:17 +0800

    feat: splash page animation

--- lib/modules/splash/views/ViewSplashContent.dart
+++ lib/modules/splash/views/ViewSplashContent.dart
@@ -17,13 +17,15 @@ class ViewSplashContent extends StatefulWidget {
   _SplashScreenState createState() => new _SplashScreenState();
 }
 
-class _SplashScreenState extends State<ViewSplashContent> with CommonTravelItem {
+class _SplashScreenState extends State<ViewSplashContent> with CommonTravelItem, SingleTickerProviderStateMixin {
 
 	BlocSplashList blocGalleryList;
 	int maxTickTime = 10;
 	int currentCountDown = 10;
 	Timer timer;
 	bool isFirstLoaded = false;
+	AnimationController controller;
+	Animation animation;
 
 	/// 倒计时
 	startTime() async {
@@ -50,11 +52,17 @@ class _SplashScreenState extends State<ViewSplashContent> with CommonTravelItem 
 	@override
 	void initState() {
 		super.initState();
+		this.controller = AnimationController(
+			vsync: this,
+			duration: Duration(seconds: 1)
+		);
+		this.animation = Tween(begin: 0.0, end: 1.0).animate(this.controller);
 		startTime();
 	}
 
 	@override
 	void dispose() {
+		this.controller.dispose();
 		this.timer.cancel();
 		super.dispose();
 		this.blocGalleryList.dispose();
@@ -78,7 +86,11 @@ class _SplashScreenState extends State<ViewSplashContent> with CommonTravelItem 
 			stream: this.blocGalleryList.outStream,
 			builder: (context, snapshot) {
 				if (snapshot.hasData) {
-					return this.buildLayout(snapshot);
+					this.controller.forward();
+					return FadeTransition(
+						opacity: this.animation, 
+						child: this.buildLayout(snapshot)
+					);
 				} else {
 					return this.buildEmptyLayout(context);
 				}
@@ -105,8 +117,8 @@ class _SplashScreenState extends State<ViewSplashContent> with CommonTravelItem 
 									child: Row(
 										mainAxisAlignment: MainAxisAlignment.end,
 										children: <Widget>[
-											Text(this.currentCountDown.toString(), style: TextStyle(color: Color(0xFFdedede), fontSize: 19.0)),
-											Text('S 跳过', style: TextStyle(color: Color(0xFFdedede), fontSize: 19.0))
+											Text(this.currentCountDown.toString(), style: TextStyle(color: Color(0xFFdedede), fontSize: 16.0)),
+											Text('S 跳过', style: TextStyle(color: Color(0xFFdedede), fontSize: 16.0))
 										],
 									),
 								),
@@ -121,7 +133,7 @@ class _SplashScreenState extends State<ViewSplashContent> with CommonTravelItem 
 								padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0),
 								width: MediaQuery.of(this.context).size.width,
 								height: 180.0,
-								child: Text(data.copyright, style: TextStyle(color: Colors.white, fontSize: 16.0)),
+								child: Text(data.copyright, style: TextStyle(color: Colors.white, fontSize: 14.0)),
 							)
 						],
 					)
